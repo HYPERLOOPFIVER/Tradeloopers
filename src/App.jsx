bce32e49aa5ae2df5b8a73606ebd5509e97d6668
hyperloopfiverr <hyperloop1254@gmail.com>
2025-01-15 19:28:53 +0530

    Added ProtectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Auth/Signup";
 import Login from "./Pages/Auth/Login";
 import Dashboard from "./Pages/Auth/Dashboard/Dashboard";
-import ProtectedRoute from "./Pages/Auth/ProtectedRoute/ProtectedRoute";
+import ProtectedRoute from "./Pages/Auth/ProtectedRoute/Protectedroute";
 import Buy from "./Pages/Auth/Dashboard/BuyStock";
 import Stocks from "./Pages/Auth/Dashboard/Stocks";
 import HomePage from "./Pages/Auth/Dashboard/Home";
 import Settings from "./Pages/Auth/Dashboard/Settings";
-import BottomNavbar from "./Pages/Auth/Dashboard/BottomNavbar";
 import FrontPage from "./Pages/Auth/Dashboard/Front";
 
 const App = () => {
