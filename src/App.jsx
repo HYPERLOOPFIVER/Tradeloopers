5050139ea26827f4da483205490aebe7ae9e1f5e
hyperloopfiverr <hyperloop1254@gmail.com>
2025-01-15 21:31:05 +0530

    Added ProtectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,21 @@ import Signup from "./Pages/Auth/Signup";
 import Login from "./Pages/Auth/Login";
 import Dashboard from "./Pages/Auth/Dashboard/Dashboard";
 import ProtectedRoute from "./Pages/Auth/ProtectedRoute/Protectedroute";
-import Buy from "./Pages/Auth/Dashboard/BuyStock";
+import Buy from "./Pages/Auth/Dashboard/Buystock";
 import Stocks from "./Pages/Auth/Dashboard/Stocks";
 import HomePage from "./Pages/Auth/Dashboard/Home";
 import Settings from "./Pages/Auth/Dashboard/Settings";
 import FrontPage from "./Pages/Auth/Dashboard/Front";
-
 const App = () => {
   return (
-    <BrowserRouter> {/* Wrapping Routes inside BrowserRouter */}
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FrontPage />} />
+      <Route path="/" element={<FrontPage/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/bsp" element={<Buy />} />
         <Route path="/stocks" element={<Stocks />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/settings" element={<Settings />} />
         <Route
           path="/dashboard"
